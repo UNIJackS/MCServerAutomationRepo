d54482af8125e71cf44d8b04155e3431397e3445
UNIJackS <unijackscrivener@gmail.com>
2026-01-08 17:14:53 +1300

    Testing stop action

--- src/App.java
+++ src/App.java
@@ -13,6 +13,8 @@ public class App {
         ServerManager serverManager = new ServerManager();
         serverManager.start();
 
+
+        serverManager.stop();
     }
 
     private static void setup() throws Exception{
