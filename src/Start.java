e34872c0d88fa62b750985520f1cfe5843734876
UNIJackS <unijackscrivener@gmail.com>
2026-01-12 02:53:20 +1300

    added 10min start delay. Allow ssever to start in screen

--- src/Start.java
+++ src/Start.java
@@ -29,15 +29,18 @@ public record Start(StatusManager statusManager) implements ServerAction {
 
     @Override
     public boolean checkActionWorked() throws InterruptedException, IOException {
+        //Loops untill the screen comes online
         for(int timeOut = 10; !(ScriptManager.status() == Status.statusEnum.ONLINE); timeOut -=1){
+            //Waits 5 seconds
             Thread.sleep(5000);
-            if(timeOut <= 0){
-                //Server has not come up
-                return false;
-            }
+
+            //If the sevrer checks 10 times and screen is not up then failed.
+            if(timeOut <= 0){return false;}
         }
 
-        Thread.sleep(5000);
+        //Wait for 10mins for sever to actually start in the screen
+        // 600000ms = 10mins
+        Thread.sleep(600000);
         return true;
 
     }
